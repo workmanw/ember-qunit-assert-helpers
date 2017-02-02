0ce1f5dd8eff897207bb6224e1ad5a44227591db
Wesley Workman <wesley.workman@gmail.com>
2017-02-02 11:09:43 -0500

    Fixed an issue that caused users to be able to fingerprint their dev builds. (#3)

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-/* jshint node: true */
+/* jshint node:true */
+/* global require, module, __dirname */
+
 'use strict';
 
+var Append = require('./lib/broccoli-append');
 var path = require('path');
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-qunit-assert-helpers',
@@ -11,31 +12,8 @@ module.exports = {
   postprocessTree: function(type, tree) {
     this._requireBuildPackages();
     if (type === 'all' && this.app.tests) {
-      // Create a tree that pulls out the test file and it's source map
-      var treeTestLoader = new Funnel(tree, {
-        files: ['test-support.js', 'test-support.map'], // don't loose the sourcemaps
-        srcDir: 'assets',
-        destDir: 'app'
-      });
-
-      // Load in the loader file from the vendor package
-      var vendor = this.treeGenerator(path.join(__dirname, 'vendor'));
-      var loader = new Funnel(vendor, {
-        files: ['ember-qunit-assert-helpers-loader.js'],
-        srcDir: '/',
-        destDir: '/assets/'
-      });
-
-      // Merge the trees and concatenate the loader into the `test-support.js`
-      var testLoaderTree = this.concatFiles(mergeTrees([treeTestLoader, loader]), {
-        inputFiles: ['**/*.js'],
-        outputFile: '/assets/test-support.js'
-      });
-
-      // Merge it all back together
-      return mergeTrees([tree, testLoaderTree], {
-        overwrite: true
-      });
+      var appendPath = path.join(__dirname, '/vendor/ember-qunit-assert-helpers-loader.js');
+      return new Append(tree, /test-support[A-Za-z0-9-]*/, appendPath);
     } else {
       return tree;
     }
