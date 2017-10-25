884baf3f4d4851df16542102e8efe140dc86f152
Alex DiLiberto <alex@alexdiliberto.com>
2017-10-25 19:48:11 -0400

    Begin conversion to RFC 232 syntax

--- tests/unit/run-loop-test.js
+++ tests/unit/run-loop-test.js
@@ -1,32 +1,35 @@
 import Ember from 'ember';
-import { test } from 'ember-qunit';
-import moduleForAssert from '../helpers/module-for-assert';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import setupAssertTest from '../helpers/setup-assert-test';
 
+module('Run Loop Assert', function(hooks) {
+  setupTest(hooks);
+  setupAssertTest(hooks);
 
-moduleForAssert('Run Loop Assert', { integration: false });
+  test('`expectNoRunLoop` in a run loop', function(assert) {
+    Ember.run.begin();
 
-test('`expectNoRunLoop` in a run loop', function(assert) {
-  Ember.run.begin();
+    assert.expectNoRunLoop();
 
-  assert.expectNoRunLoop();
+    // Restore the asserts (removes the mocking)
+    this.restoreAsserts();
 
-  // Restore the asserts (removes the mocking)
-  this.restoreAsserts();
-
-  assert.notOk(this.pushedResults[0].result, '`expectNoRunLoop` detected active runloop');
-  assert.notOk(Ember.run.currentRunLoop, 'ends run loop');
-});
-
-test('`expectNoRunLoop` when timers are active', function(assert) {
-  Ember.run.later(function() {
-    assert.ok(false, 'should not execute');
+    assert.notOk(this.pushedResults[0].result, '`expectNoRunLoop` detected active runloop');
+    assert.notOk(Ember.run.currentRunLoop, 'ends run loop');
   });
 
-  assert.expectNoRunLoop();
+  test('`expectNoRunLoop` when timers are active', function(assert) {
+    Ember.run.later(function() {
+      assert.ok(false, 'should not execute');
+    });
 
-  // Restore the asserts (removes the mocking)
-  this.restoreAsserts();
+    assert.expectNoRunLoop();
 
-  assert.notOk(this.pushedResults[0].result, '`expectNoRunLoop` detected active runloop');
-  assert.notOk(Ember.run.hasScheduledTimers(), 'ends run loop');
+    // Restore the asserts (removes the mocking)
+    this.restoreAsserts();
+
+    assert.notOk(this.pushedResults[0].result, '`expectNoRunLoop` detected active runloop');
+    assert.notOk(Ember.run.hasScheduledTimers(), 'ends run loop');
+  });
 });
