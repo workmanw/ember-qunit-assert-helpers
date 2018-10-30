aaaf8f034510b8d4e9e64595a372ff38a65b8c72
gossi <gossi@users.noreply.github.com>
2018-10-30 11:46:17 +0100

    lazy load the installers, get rid of the other stuff

--- vendor/ember-qunit-assert-helpers-loader.js
+++ vendor/ember-qunit-assert-helpers-loader.js
@@ -1,4 +1,8 @@
 /* globals require */
 
 // Loading the file here will install the asserts onto `QUnit.Assert` class.
-require('ember-qunit-assert-helpers/test-support/install-qunit-asserts');
+// 'ember-qunit-assert-helpers/test-support/install-qunit-asserts' is not available at the current cycle
+// delay the require onto the next tick until all lazy loaded require statements are resolved
+Promise.resolve().then(() => {
+  require('ember-qunit-assert-helpers/test-support/install-qunit-asserts');
+});
